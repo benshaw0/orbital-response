8d7c7365d72093d9047eead7961101696f7c2c90
Christian Miro Segui <cmirosegui@gmail.com>
2025-06-03 11:02:30 +0200

    blank notebooks added for members

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if "git+" not in x]
+
+setup(name='orbital_response',
+      version="0.0.01",
+      description="Orbital Response Project",
+      license="MIT",
+      author="Ben Shaw, Christian Miró, Felix Pless",
+      author_email="",
+      #url="https://github.com/lewagon/taxi-fare",
+      install_requires=requirements,
+      packages=find_packages(),
+      test_suite="tests",
+      # include_package_data: to install data from MANIFEST.in
+      include_package_data=True,
+      zip_safe=False)
