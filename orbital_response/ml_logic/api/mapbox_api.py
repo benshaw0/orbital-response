78378097e385ce3cd9f886930815cf827564c685
Christian Miro Segui <cmirosegui@gmail.com>
2025-06-04 17:28:46 +0200

    working mapbox api for pre images

---
+++ orbital_response/ml_logic/api/mapbox_api.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+import os
+import requests
+from PIL import Image
+from io import BytesIO
+
+lat, lon = 31.55195, 34.49948
+zoom = 17
+size = (1024, 1024)
+mapbox_token = os.getenv("MAPBOX_API_KEY")
+
+url = f"https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/{lon},{lat},{zoom}/{size[0]}x{size[1]}?access_token={mapbox_token}"
+
+r = requests.get(url)
+if r.ok:
+    img = Image.open(BytesIO(r.content))
+    img.save("orbital_response/ml_logic/api/images/pre_disaster.png")
+    print("Saved pre image .png")
+else:
+    print("Error when downloading pre image:", r.status_code)
