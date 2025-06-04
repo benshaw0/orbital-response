66f4bda9cd74779ee8fa67953510363290abb9a3
Christian Miro Segui <cmirosegui@gmail.com>
2025-06-04 17:40:43 +0200

    .env working fine now

--- orbital_response/ml_logic/api/mapbox_api.py
+++ orbital_response/ml_logic/api/mapbox_api.py
@@ -4,6 +4,8 @@ import requests
 from PIL import Image
 from io import BytesIO
 
+load_dotenv(dotenv_path=".env")
+
 lat, lon = 31.55195, 34.49948
 zoom = 17
 size = (1024, 1024)
@@ -14,7 +16,9 @@ url = f"https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/{lon},{lat},
 r = requests.get(url)
 if r.ok:
     img = Image.open(BytesIO(r.content))
-    img.save("orbital_response/ml_logic/api/images/pre_disaster.png")
+    img.save("pre_disaster.png")
     print("Saved pre image .png")
 else:
     print("Error when downloading pre image:", r.status_code)
+
+print(mapbox_token)
