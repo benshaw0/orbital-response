21eec17a5d7dfb0fe3d99a551b8f1f2da598fa8b
Christian Miro Segui <cmirosegui@gmail.com>
2025-06-04 17:30:23 +0200

    preprocessor now deletes old splits if theres any

--- orbital_response/ml_logic/preprocessing/preprocessor.py
+++ orbital_response/ml_logic/preprocessing/preprocessor.py
@@ -1,15 +1,21 @@
+import shutil
+
+
 from src.preprocessing.convert_tif_to_png import convert_all_tifs
 from src.preprocessing.generate_masks_from_png import generate_masks_from_png
 from src.preprocessing.split_dataset import split_dataset
 
 
 if __name__ == "__main__":
+
+    ##### GET DATASET TIF AND CONVERT THEM TO PNG
     print("Converting .tif to .png")
     convert_all_tifs(
         source_root="data",
         output_dir="data/processed/images"
     )
 
+    ##### GET THE RGB MASKS FROM JSON USING IMAGES 1024x1024 FORMAT
     print("using Json files to create the masks")
     generate_masks_from_png(
         png_dir="data/processed/images",
@@ -17,12 +23,20 @@ if __name__ == "__main__":
         output_dir="data/processed/masks"
     )
 
+    ##### REMOVE SPLIT OLD FOLDER IF ANY #####
+    print("Cleaning old split files")
+    try:
+        shutil.rmtree("data/processed/split")
+    except FileNotFoundError:
+        pass
+
+    ##### SPLIT HOLDOUT METHOD #####
     print("Splitting the data in train/test/val")
     split_dataset(
         image_dir="data/processed/images",
         mask_dir="data/processed/masks",
         output_dir="data/processed/split",
-        dataset_fraction=1.0,
+        dataset_fraction=0.15,
         train_pct=0.7,
         val_pct=0.15,
         test_pct=0.15
