46762572f20b9936b837ded708885dd9ca7beea0
Christian Miro Segui <cmirosegui@gmail.com>
2025-06-05 09:45:37 +0200

    created the function working

--- orbital_response/ml_logic/api/goog_static_map_api.py
+++ orbital_response/ml_logic/api/goog_static_map_api.py
@@ -6,22 +6,24 @@ from io import BytesIO
 
 load_dotenv(dotenv_path=".env")
 
-lat, lon = 31.551953, 34.49948
-zoom = 17
-size="512x512"
-scale=2
-api_key = os.getenv("GOOGLE_API_KEY")
+def google_api(lat, lon):
+    zoom = 17
+    size="512x512"
+    scale=2
+    api_key = os.getenv("GOOGLE_API_KEY")
 
-url = (
-    f"https://maps.googleapis.com/maps/api/staticmap?"
-    f"center={lat},{lon}&zoom={zoom}&size={size}&scale={scale}&maptype=satellite&key={api_key}"
-)
+    url = (
+        f"https://maps.googleapis.com/maps/api/staticmap?"
+        f"center={lat},{lon}&zoom={zoom}&size={size}&scale={scale}&maptype=satellite&key={api_key}"
+    )
 
 
-r = requests.get(url)
-if r.ok:
-    img = Image.open(BytesIO(r.content))
-    img.save("post_disaster.png")
-    print("Saved post image.png")
-else:
-    print("Error when downloading post image:", r.status_code)
+    r = requests.get(url)
+    if r.ok:
+        img = Image.open(BytesIO(r.content))
+        img.save("post_disaster.png")
+        print("Saved post image.png")
+    else:
+        print("Error when downloading post image:", r.status_code)
+
+google_api(31.55195, 34.49948)
