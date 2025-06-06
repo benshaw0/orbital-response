ac70fe943b28158304e6c90480463c590f34665e
Felix Pless <felix.fpless@user.noreply.github.com>
2025-06-06 13:22:06 +0200

    streamlit_UI_3 working images

--- streamlit/app_ui.py
+++ streamlit/app_ui.py
@@ -14,9 +14,10 @@ from orbital_response.ml_logic.api.goog_static_map_api import google_api
 # Importing MapBox API
 from orbital_response.ml_logic.api.mapbox_api import mapbox_api
 
-from dotenv import load_dotenv  # Make sure this is imported
-# ✅ Load environment variables before any getenv call
-load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "../../.env"))
+# Importing Bens Model
+from WHATEVER import BENS MODEL
+
+from dotenv import load_dotenv
 
 from geopy.distance import geodesic
 from streamlit_folium import st_folium
@@ -39,7 +40,7 @@ st.title("Orbital Response Damage Predictor")
 # 
 warzone = {
     "-": ["-"],
-    "Europe": ["-", "Ukraine", "Turkey"],
+    "Europe": ["-", "Spain","Ukraine", "Turkey"],
     "Middle East": ["-", "Lebanon", "Syria", "Israel", "Iran", "Yemen"],
     "Africa": ["-", "Libya", "Sudan"]
 }
@@ -83,70 +84,122 @@ if response.status_code == 200:
 # Initiating Columns
 # 
 #    
-    if country_shape:
-        #col1, col2 = st.columns([1, 1], gap="medium")
+#if country_shape:
+    col1, col2, col3 = st.columns([1, 1, 1], gap="medium")
 #
 #
 # 1st Column
 #
 #
-        #with col1:
-            map_height = 550
-            map_width = 550
-            user_map = folium.Map(zoom_start=6)
-            bounds = folium.GeoJson(country_shape).get_bounds()
-            user_map.fit_bounds(bounds)
+    with col1:
+        map_height = 800
+        map_width = 800
+        user_map = folium.Map(zoom_start=6)
+        bounds = folium.GeoJson(country_shape).get_bounds()
+        user_map.fit_bounds(bounds)
 
-            folium.GeoJson(
-                country_shape,
-                style_function=lambda x: {
-                    'fillOpacity': 0,
-                    'color': 'red',
-                    'weight': 3
-                }
-            ).add_to(user_map)
-            user_map.add_child(folium.LatLngPopup())
-            click_data = st_folium(user_map, width=map_width, height=map_height, returned_objects=["last_clicked"])
+        folium.GeoJson(
+            country_shape,
+            style_function=lambda x: {
+                'fillOpacity': 0,
+                'color': 'red',
+                'weight': 3
+            }
+        ).add_to(user_map)
+        user_map.add_child(folium.LatLngPopup())
+        click_data = st_folium(user_map, width=map_width, height=map_height, returned_objects=["last_clicked"]) # After clicking the "returned object" of lat and lon is stored in last_clicked
 #
 #
 # 2nd Column
 #
 #
-        #with col2:
-            if click_data and click_data["last_clicked"]:
-                lat = click_data["last_clicked"]["lat"]
-                lon = click_data["last_clicked"]["lng"]
-                st.session_state["lat"] = lat
-                st.session_state["lon"] = lon
+    with col2:
+        if click_data and click_data["last_clicked"]:
+            lat = click_data["last_clicked"]["lat"]
+            lon = click_data["last_clicked"]["lng"]
+            st.session_state["lat"] = lat
+            st.session_state["lon"] = lon
 
-                mapbox_api(lat, lon)
-
-                image_path = "streamlit/pre_disaster.png"
-                if os.path.exists(image_path):
-                    st.image(image_path, width=800, clamp=True)
-    #            else:
-    #                 st.error("Satellite image could not be loaded. Check your API key or network connection.")
-    #         else:
-    #             st.markdown("### Satellite image will appear here after map interaction.")
-    # else:
-    #     st.error("Could not find shape data for this country.")
-    
-    # st.success("WHATEVER YOU LITTLE BITCH")
-
-            if click_data and click_data["last_clicked"]:
-                lat = click_data["last_clicked"]["lat"]
-                lon = click_data["last_clicked"]["lng"]
-                st.session_state["lat"] = lat
-                st.session_state["lon"] = lon
-
-                mapbox_api(lat, lon)
-
-                image_path = "streamlit/pre_disaster.png"
-                if os.path.exists(image_path):
-                    st.image(image_path, width=800, clamp=True)
-                else:
-                    st.error("Satellite image could not be loaded. Check your API key or network connection.")
+            url = mapbox_api(lat, lon)
+            image_path = "/home/felix/code/benshaw0/orbital-response/streamlit/pre_disaster.png"
+            if os.path.exists(image_path):
+                st.image(image_path, width=800, clamp=True)
+#
+#
+# 3rd Column
+#
+#
+    with col3:
+            google_api(lat, lon)
+            image_path = "/home/felix/code/benshaw0/orbital-response/streamlit/post_disaster.png"
+            if os.path.exists(image_path):
+                st.image(image_path, width=800, clamp=True)
             else:
-                st.markdown("### Satellite image will appear here after map interaction.")
-    else:
-        st.error("Could not find shape data for this country.")
+                st.error("Satellite image could not be loaded. Check your API key or network connection.")
+#
+# 
+# Input of the Model of Ben / Masking etc.
+# 
+#                    
+    if click_data and click_data["last_clicked"]:
+        st.markdown("---")
+        st.info("Here is supposed to be displayed the Model of Ben")
+
+
+        trained_model_1 = UNetModel(n_classes=5).to(device) 
+        trained_model_1.load_state_dict(torch.load(model_pth, map_location=torch.device(device)))
+
+        BENS FUNCTION(lat, lon)
+        concacinated_images = "path"
+        st.image(concacinated_images, width=800, clamp=True)
+
+
+# google_api(lat, lon)
+# image_path = "/home/felix/code/benshaw0/orbital-response/streamlit/post_disaster.png"
+# if os.path.exists(image_path):
+# st.image(image_path, width=800, clamp=True)
+
+
+
+###################################################################################################################
+#model_pth = '/Users/BenedictShaw/code/benshaw0/orbital-response/orbital_response/models/unet_bs001.pth'
+#trained_model_1 = UNetModel(n_classes=5).to(device) /// calls the model
+#trained_model_1.load_state_dict(torch.load(model_pth, map_location=torch.device(device))) /// params
+######################################################################################################################
+###### images for test the model
+# pre_path = "/home/felix/code/benshaw0/orbital-response/streamlit/pre_disaster.png"
+# post_path = /home/felix/code/benshaw0/orbital-response/streamlit/post_disaster.png"
+# pre_img = Image.open(pre_path)
+# post_img = Image.open(post_path)
+# img_transform = transforms.Compose([
+#     transforms.Resize((224, 224)),
+#     transforms.ToTensor()
+# ])
+# # Apply transform and concat
+# pre_tensor = img_transform(pre_img)
+# post_tensor = img_transform(post_img)
+# input_tensor = torch.cat([pre_tensor, post_tensor], dim=0).unsqueeze(0).to(device)  # [1, 6, 224, 224]
+# output_tensor = trained_model_1(input_tensor)
+# ##############################################################################################
+# CLASS_COLORS = {
+#     0: (0, 0, 0),         # un-classified (black)
+#     1: (0, 255, 0),       # no-damage (green)
+#     2: (0, 0, 255),     # minor-damage (blue)
+#     3: (255, 165, 0),     # major-damage (orange)
+#     4: (255, 0, 0),       # destroyed (red)
+# }
+# def mask_to_rgb(mask_tensor):
+#     mask_np = mask_tensor.cpu().numpy()
+#     h, w = mask_np.shape
+#     rgb = np.zeros((h, w, 3), dtype=np.uint8)
+#     for class_idx, color in CLASS_COLORS.items():
+#         rgb[mask_np == class_idx] = color
+#     return Image.fromarray(rgb)
+##################################################
+######### Dispalying images
+# model.eval()
+# with torch.no_grad():
+#     output = model(input_tensor)  # [1, 5, H, W]
+#     pred_mask = torch.argmax(output, dim=1)[0]  # [H, W]
+# # prediction to RGB
+# rgb_mask = mask_to_rgb(pred_mask)
